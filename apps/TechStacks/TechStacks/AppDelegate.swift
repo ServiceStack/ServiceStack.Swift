7621f9b59a8b8982bfb10e7e5e07d7769f44c970
Demis Bellot <demis.bellot@gmail.com>
2015-02-08 10:19:28 -0500

    Reset caches when we receive a memory warning

--- apps/TechStacks/TechStacks/AppDelegate.swift
+++ apps/TechStacks/TechStacks/AppDelegate.swift
@@ -48,6 +48,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func applicationDidReceiveMemoryWarning(application: UIApplication) {
+        appData.resetCache()
+    }
 
     var appData = AppData()
 }
