c0ba0578ddd47288ab2e927643d3e8be13ee539e
Alfonso Grillo <alfogrillo@gmail.com>
2020-07-02 18:15:40 +0200

    Update package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,8 +13,10 @@ let package = Package(
         ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .Package(url: "https://github.com/mxcl/PromiseKit.git", versions: Version(6, 8, 0)..<Version(6, 8, 3))
+        .package(url: "https://github.com/mxcl/PromiseKit.git", Version(6, 8, 0)..<Version(6, 8, 3))
     ],
-    exclude: ["Tests"]
+    targets: [
+        .target(name: "ServiceStack", dependencies: ["PromiseKit"], path: "Sources"),
+        .testTarget(name: "ServiceStackTests", dependencies: ["ServiceStack"], path: "Tests")
+    ]
 )
