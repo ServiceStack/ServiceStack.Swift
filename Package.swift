57d5d9888cc399bdc76faa9b7df672b8a0ad85d2
Alfonso Grillo <alfogrillo@gmail.com>
2020-07-02 19:32:38 +0200

    Cleanup manifest

--- Package.swift
+++ Package.swift
@@ -7,10 +7,8 @@ let package = Package(
     name: "ServiceStack",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "ServiceStack",
-            targets: ["ServiceStack"]),
-        ],
+        .library(name: "ServiceStack", targets: ["ServiceStack"])
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/mxcl/PromiseKit.git", Version(6, 8, 0)..<Version(6, 13, 2))
